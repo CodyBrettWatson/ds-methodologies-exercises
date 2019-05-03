85c3a3c7fcabece2ef5db4a852ae7b93cb9eb034
cody watson <codywatson@codys-MacBook-Pro.local>
2019-05-03 07:29:57 -0500

    add

---
+++ tsa/prepare.py
@@ -0,0 +1,26 @@
+# Prepare
+# prepare.py
+# Write a function to convert a date to 
+#   adatetime data type.
+# Write a function to change a 
+#   datetime to UTC.
+# Write a function to parse a date column 
+#   into 6 additional columns (while 
+#   keeping the original date): year, 
+#   quarter, month, day of month, day of 
+#   week, weekend vs. weekday
+# Add a column to your dataframe, 
+#   sales_total, which is a derived 
+#   from sale_amount (total items) and 
+#   item_price.
+# Create a new dataframe that aggregates 
+#   the sales_total and sale_amount(item 
+#   count) using sum and median by day of 
+#   week.
+# Explore the pandas DataFrame.diff() 
+#   function. Create a new column that is 
+#   the result of the current sales - the 
+#   previous days sales.
+# Write a function to set the index to 
+#   be the datetime variable.
+
